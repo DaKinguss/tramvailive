1a7d1808f0d130f3d8986ad880adf477a57a04e3
DaKinguss <131606062+DaKinguss@users.noreply.github.com>
2026-09-14 10:25:09 +0300

    Delete src directory

--- src/lib/stpt/carto.server.ts
+++
@@ -1,8 +0,0 @@
-import { env } from "@/lib/env.server";
-
-const CARTO_API_KEY = env("CARTO_API_KEY") ?? "cb1_3fk0_1_c40c96719d53939eb11470e0";
-
-export function cartoDarkTileUrl(z: number, x: number, y: number): string {
-  const key = encodeURIComponent(CARTO_API_KEY);
-  return `https://basemaps.cartocdn.com/rastertiles/dark_all/${z}/${x}/${y}@2x.png?key=${key}`;
-}
